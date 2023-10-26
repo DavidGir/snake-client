9eebe3805943d7ff0fb1693d28a790d2be57271e
David Giroux <DavidGiroux149@gmail.com>
2023-10-25 20:21:25 -0400

    conducted move messages experiments

--- client.js
+++ client.js
@@ -5,10 +5,9 @@ const config = {
   host: "165.227.47.243 "
 };
 
-// establishes a connection with the game server
+// Callback function that establishes a connection with the game server:
 const connect = function() {
   const conn = net.createConnection(config);
-
   // interpret incoming data as text
   conn.setEncoding("utf8");
   // Get a print when connection is successfully made:
@@ -21,41 +20,25 @@ const connect = function() {
   conn.on("data", (data) => {
     console.log("Server says:", data);
   });
-
   return conn;
 };
 
+// Callback function that establishes movement commands with the server:
+const commands = function() {
+  const conn = net.createConnection(config);
+  // If we send more tha one move message:
+  // conn.write("Move: up");
+  // conn.write("Move: down");
+  // If we send more than one move with delay:
+  // setTimeout(() => conn.write("Move: up"), 50);
+  // setTimeout(() => conn.write("Move: down"), 100);
+  // Gets out the game server frame:
+  // setInterval(() => conn.write("Move: up"), 50);
+};
+
 console.log("Connecting ...");
-connect();
+// connect();
+commands();
 
 
 module.exports = { connect };
-
-
-// const net = require('net');
-
-
-
-// const connection = net.createConnection(config);
-
-// // set the encoding on the connection to interpret binary as text:
-// connection.setEncoding("utf-8");
-
-// // send message to server:
-// connection.write("Hello from client");
-
-
-// set the encoding on stdin:
-// process.stdin.setEncoding("utf-8");
-
-// // listen for things typed in terminal/ stdin:
-// process.stdin.on("data", (data) => {
-//   // data = data.trim();
-
-//   // if (data === "a") {
-//   //   connection.write("move: left");
-//   // }
-//   //send the data to the server:
-//   connection.write(data);
-
-// });
