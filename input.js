08813b0ac39f4e7d5383f7ddbb0526d7e016c36a
David Giroux <DavidGiroux149@gmail.com>
2023-10-25 22:41:27 -0400

    added exports in entry point page and code

--- input.js
+++ input.js
@@ -1,7 +1,12 @@
+const { connect } = require("./client");
 const net = require("net");
 
+
+let connection;
+
 // Callback to handle user input from stdin:
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   // Allows to listen to terminal input:
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -13,11 +18,27 @@ const setupInput = function() {
 
 const handleUserInput = function(key) {
   // Handles when user inputs ctrl+c to kill connection:
-  if (key === '\u0003') {
+  if (key === "\u0003") {
     process.exit();
+  }
+  if (key === "w") {
+    // console.log("Move: up");
+    connection.write("Move: up");
+  }
+  if (key === "s") {
+    // console.log("Move: down");
+    connection.write("Move: down");
+  }
+  if (key === "d") {
+    // console.log("Move: right");
+    connection.write("Move: right");
+  }
+  if (key === "a") {
+    // console.log("Move: left");
+    connection.write("Move: left");
   }
 };
 
-setupInput();
+// setupInput();
 
 module.exports = { setupInput };
